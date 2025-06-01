96c72575680f4244db7d76e6983906d0410c7aab
wendelca <vvkamalov_obs@mail.ru>
2025-06-01 16:43:51 +0500

    eureka: add eureka server module

--- eureka-server/src/test/java/com/github/tennyros/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/github/tennyros/eurekaserver/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.github.tennyros.feigneureka;
+package com.github.tennyros.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FeignEurekaApplicationTests {
+class EurekaServerApplicationTests {
 
 	@Test
 	void contextLoads() {
